aea2f14a2bd24431f26a3ea2786c533f7cf76908
DESKTOP-98AMS46\Amin <972839577@qq.com>
2022-05-03 18:32:55 +0900

    更新打包配置

--- config/prod.webpack.js
+++ config/prod.webpack.js
@@ -6,7 +6,7 @@ module.exports = {
         mode: 'production',
         entry: path.join(__dirname, '../src/index.ts'),
         output: {
-            path: path.join(__dirname, '../dist'),
+            path: path.join(__dirname, '../docs'),
             filename: '[name].js'
         },
         module: {
